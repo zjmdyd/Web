ffc9e52147dbd81818baef6dec9d98c648ec92fe
ZJ <ZJ@asssi.local>
2018-08-24 21:14:09 +0800

    aa

--- 02_js/02_js动画/04_animate.js
+++ 02_js/02_js动画/04_animate.js
@@ -11,22 +11,20 @@ function animate(el, propertys) {
 
 	el.timerId = setInterval(function() {
 		for (var property in propertys) {
-		var current;
-		var target = propertys[property]
-		if (property === 'opacity') {
-			current = Math.round(parseFloat(getStyle(el, property)) * 100);
-		}else {
-			current = parseInt(getStyle(el, property));
+			var current;
+			var target = propertys[property]
+			if (property === 'opacity') {
+				current = Math.round(parseFloat(getStyle(el, property)) * 100);
+			}else {
+				current = parseInt(getStyle(el, property));
+			}
+			var speed = (target - current) / 30;
+			speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);
+			if (property === 'opacity') {
+				el.style[property] = (current + speed)/100;
+			}else {
+				el.style[property] = current + speed + 'px';
+			}
 		}
-		var speed = (target - current) / 30;
-		speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);
-		if (property === 'opacity') {
-			el.style[property] = (current + speed)/100;
-		}else {
-			el.style[property] = current + speed + 'px';
-		}
-	}
-
-		
 	}, 20);
 }
