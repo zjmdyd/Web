eeb8e1742c1030e6bfeb4030797c4a9468b8ba6c
zengjian2 <jianzengj@isoftstone.com>
2022-12-30 18:16:25 +0800

    add

---
+++ 05_vue3/vue-demo-02/src/components/ButtonCounter.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <button @click="count++">You clicked me {{ count }} times.</button>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0)
+</script>
---
+++ 05_vue3/vue-demo-02/src/components/vue3-009.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>Here is a child components</h1>
+    <!-- 组件可以被重用任意多次 -->
+    <!-- 你会注意到，每当点击这些按钮时，每一个组件都维护着自己的状态，
+        是不同的 count。这是因为每当你使用一个组件，就创建了一个新的实例 -->
+    <ButtonCounter />
+    <ButtonCounter />
+    <ButtonCounter />
+</template>
+
+<script setup>
+
+import ButtonCounter from './ButtonCounter.vue'
+
+</script>
